e97e732512be4fe76c61b7efde1ed908788300c1
Josh Quintus <josh.quintus@gmail.com>
2017-10-17 22:58:21 -0400

    Add some unit tests

--- FSpike.Tests/Program.fs
+++ FSpike.Tests/Program.fs
@@ -1,7 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open Fuchu
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv = defaultMainThisAssembly argv
+    //printfn "%A" argv
+    //0 // return an integer exit code
---
+++ FSpike.Tests/SimpleTests.fs
@@ -0,0 +1,26 @@
+﻿module SimpleTests
+
+open Fuchu
+
+let alwaysTrue x = true
+let head array = 
+    array |> Array.find alwaysTrue
+
+[<Tests>]
+let tests = 
+    testList "A test group" [
+        testCase "one test" <|
+            fun _ -> Assert.Equal("2+2", 4, 2+2)
+        testCase "another test" <|
+            fun _ -> Assert.Equal("3+3", 3, 3+3)
+    ]
+
+[<Tests>]
+let tests2 =
+    testList "My Simple Tests" [
+        testCase "Arrays" <|
+            fun _ -> 
+                let myHead = [| 1; 2; 3 |] |> head
+                Assert.Equal("head of [| 1; 2; 3 |]", 1, myHead)
+
+    ]

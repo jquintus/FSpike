6e5f4a12538773c16941e17450591e83f18fff60
Josh <josh.quintus@gmail.com>
2017-10-18 23:11:46 -0400

    Fix failing test (#2)

--- FSpike.Tests/SimpleTests.fs
+++ FSpike.Tests/SimpleTests.fs
@@ -12,7 +12,7 @@ let tests =
         testCase "one test" <|
             fun _ -> Assert.Equal("2+2", 4, 2+2)
         testCase "another test" <|
-            fun _ -> Assert.Equal("3+3", 3, 3+3)
+            fun _ -> Assert.Equal("3+3", 6, 3+3)
     ]
 
 [<Tests>]

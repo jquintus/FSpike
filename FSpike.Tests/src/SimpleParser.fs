01b70b6fed54631d9bc2184bca4830ab8ce6ddd6
Josh Quintus <josh.quintus@gmail.com>
2017-10-20 01:19:43 -0400

    Combining parsers

--- FSpike.Tests/src/SimpleParser.fs
+++ FSpike.Tests/src/SimpleParser.fs
@@ -12,8 +12,8 @@ open Swensen.Unquote
 let str s =pstring s
 let floatBetweenBrackets = str "[" >>. pfloat .>> str "]"
 let intAfterGreaterThans = str ">" >>. str ">" >>. pint32
-
-
+let manyFloats = many floatBetweenBrackets
+let manyFloatsSeperatedByCommas = many (str ">") >>. sepBy floatBetweenBrackets (str ", ")
 
 let startsWith input (s:string) = s.StartsWith(input)
 
@@ -22,27 +22,32 @@ let testParser' p str =
     | Success(result, _, _)   -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Error: %A" errorMsg
 
-let testParser p str = 
+let getSuccessResult p str = 
     match run p str with
     | Success(result, _, _)   -> Some result
     | Failure(_, _, _)        -> None
+let getFailureMessage p str =
+    match run p str with
+    | Success(_, _, _)   -> None
+    | Failure(errorMsg, _, _) -> Some errorMsg
+    
 
 [<Tests>]
 let parserTests = 
     testList "Parser Tests" [
         testCase "int parsed as float succeeds" <|
             fun _ ->
-                testParser pfloat "1" =! Some 1.0
+                getSuccessResult pfloat "1" =! Some 1.0
                 ()
 
         testCase "non number parsed as float fails" <|
             fun _ ->
-                testParser pfloat "hello world" =! None
+                getSuccessResult pfloat "hello world" =! None
                 ()
 
         testCase "Float parsed as float succeeds" <|
             fun _ ->
-                test <@ testParser pfloat "1.24" = Some 1.24 @>
+                test <@ getSuccessResult pfloat "1.24" = Some 1.24 @>
     ]
 
 [<Tests>]
@@ -50,10 +55,21 @@ let parserTests' =
     testList "Parser Float With Brackets" [
         testCase "Float between brackets succeeds" <|
             fun _ -> 
-                test <@ testParser floatBetweenBrackets "[1.42]" = Some 1.42 @>
+                test <@ getSuccessResult floatBetweenBrackets "[1.42]" = Some 1.42 @>
         testCase "Float without brackets fails" <|
             fun _ -> 
-                test <@ testParser floatBetweenBrackets "1.42" = None @>
+                test <@ getSuccessResult floatBetweenBrackets "1.42" = None @>
+        testCase "Many Floats within bracketss succeed" <|
+            fun _ -> 
+                test <@ getSuccessResult manyFloats "[1.42][3.14]" = Some [ 1.42; 3.14 ] @>
+                        
+        testCase "Empty string returns empty list" <|
+            fun _ -> 
+                test <@ getSuccessResult manyFloats "" = Some [ ] @>
+
+        testCase "Comma Sepperated Floats succeed" <|
+            fun _ -> 
+                test <@ getSuccessResult manyFloatsSeperatedByCommas  ">>>[1.42], [3.14]" = Some [ 1.42; 3.14 ]  @>
     ]
 
 [<Tests>]
@@ -61,5 +77,5 @@ let parserTests'' =
     testList ">> Int Parser" [
         testCase ">> 45 returns 45" <|
             fun _ -> 
-                test <@ testParser intAfterGreaterThans ">>45" = Some 45 @>
+                test <@ getSuccessResult intAfterGreaterThans ">>45" = Some 45 @>
     ]

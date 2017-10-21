a3e64467016ddfefc93e49aca4cfbb0832555ceb
Josh Quintus <josh.quintus@gmail.com>
2017-10-21 11:29:20 -0400

    Parsing repeated strings.

--- FSpike.Tests/src/StringParsers.fs
+++ FSpike.Tests/src/StringParsers.fs
@@ -1,4 +1,5 @@
-﻿module StringParsers
+﻿// reference:  http://www.quanttec.com/fparsec/tutorial.html#parsing-a-float-between-brackets
+module StringParsers
 
 open ParsingTestingUtils
 open Fuchu
@@ -10,10 +11,35 @@ open FParsec
 let str s = pstring s
 let ws = spaces
 
+// helpers 
+
 [<Tests>]
 let parserTests = 
     testList "String Parser" [
-        t "???" <|
+        t "Many orrred together returns list of each item" <|
             fun _ -> 
-                test <@ 34 = 34 @>
+                // Assemble
+                let pA = str "a" <|> str "A"
+                let pB = str "b" <|> str "B"
+                let parser = many ( pA <|> pB)
+
+                let expected = [ "a"; "a"; "A"; "B"; "b"; "A"; "a"  ]
+
+                test <@ getSuccessResult parser "aaABbAa" = Some expected @>
+
+        t "Skip string" <|
+            fun _ ->
+                let parser = skipStringCI "hello " >>. pstringCI "world"
+                test <@ getSuccessResult parser "HeLlO WORLD" = Some "WORLD" @>
+
+        t "Skip string repeated" <|
+            fun _ -> 
+                // Assemble
+                let pA = str "a" <|> str "A"
+                let pB = skipStringCI "b"
+                let parser = many ( pB >>. pA)
+
+                let expected = [ "a"; "A";  ]
+
+                test <@ getSuccessResult parser "baBA" = Some expected @>
     ]

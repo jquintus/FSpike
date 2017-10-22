8a3725b7a5fd5722c166d894ce9283dd3ba9d8e9
Josh Quintus <josh.quintus@gmail.com>
2017-10-22 12:56:32 -0400

    Add tests cases for more complex scenarios
    * lists
    * multiple keys

--- FSpike.Tests/src/JsonTester.fs
+++ FSpike.Tests/src/JsonTester.fs
@@ -14,34 +14,40 @@ let testJson' input =
     let result = run json input |> result2Str
     test <@ result = "dummy" @>
 
-
 let mapJsonTests = mapTests1 (getSuccessResult json) Some
 
-let testInput = [
-    ("True Parses",    @"{ ""Key"" : true }",        JObject (Map [("Key", JBool(true))]) )
-    ("False Parses",   @"{ ""Key"" : false }",       JObject (Map [("Key", JBool(false))]) )
-                                                     
-    ("Null Parses",    @"{ ""Key"" : null }",        JObject (Map [("Key", JNull)]) )
-                                                     
-    ("int Parses",     @"{ ""Key"" : 42 }",          JObject (Map [("Key", JNumber (42.0))]) )
-    ("float Parses",   @"{ ""Key"" : 4.2 }",         JObject (Map [("Key", JNumber (4.2))]) )
+let MapJObject value = JObject (Map value)
 
-    ("String Parses",  @"{ ""Key"" : ""hello"" }",   JObject (Map [("Key", JString("hello"))]) )
+
+let testInput = [
+    ("True Parses",    @"{ ""Key"" : true }",        MapJObject [("Key", JBool(true))] )
+    ("False Parses",   @"{ ""Key"" : false }",       MapJObject [("Key", JBool(false))] )
+                                                     
+    ("Nulls Parse",    @"{ ""Key"" : null }",        MapJObject [("Key", JNull)] )
+                                                     
+    ("ints Parse",     @"{ ""Key"" : 42 }",          MapJObject [("Key", JNumber (42.0))] )
+    ("floats Parse",   @"{ ""Key"" : 4.2 }",         MapJObject [("Key", JNumber (4.2))] )
+
+    ("Strings Parse",  @"{ ""Key"" : ""hello"" }",   MapJObject [("Key", JString("hello"))] )
+
+    ("MultipleValues Parse",  
+                       @"{ ""Key1"" : ""hello"",
+                           ""Key2"" : ""World""
+                       }",   
+                                                     MapJObject [
+                                                        ("Key1", JString("hello"))
+                                                        ("Key2", JString("World"))
+                                                        ] )
+    ("Lists Parse",
+                        @"{ ""bools"" : [true,  true, false] }",
+                                                     MapJObject [
+                                                        ("bools", JList ( [ 
+                                                                            JBool(true)
+                                                                            JBool(true)
+                                                                            JBool(false) ]))
+                                                        ] )
 ]
 
 [<Tests>]
 let parserTests' = 
     testList "JSON Parser" ( mapJsonTests testInput )
-
-[<Tests>]
-let parserTests = 
-    testList "JSON Parser" [
-
-        //t "list parses" <|
-        //    fun _ -> 
-        //        let input = @"{ ""Key"" : [""A"" : ""B""] }"
-        //        let expected = JObject (Map [("Key", JList Map [("A", "B")])])
-
-        //        testJson input expected
-
-]
